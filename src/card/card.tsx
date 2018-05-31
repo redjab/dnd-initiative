34e7bc3f11788afd411cf781930356947295a4b2
Minh Do <minhd@xactware.com>
2018-05-31 17:26:49 -0600

    add card

---
+++ src/card/card.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react';
+
+class PlayCard extends React.Component {
+	public render() {
+		return (
+			<Card>
+			<Image src='/assets/images/avatar/large/matthew.png' />
+			<Card.Content>
+			  <Card.Header>
+				Matthew
+			  </Card.Header>
+			  <Card.Meta>
+				<span className='date'>
+				  Joined in 2015
+				</span>
+			  </Card.Meta>
+			  <Card.Description>
+				Matthew is a musician living in Nashville.
+			  </Card.Description>
+			</Card.Content>
+			<Card.Content extra={true}>
+			  <a>
+				<Icon name='user' />
+				22 Friends
+			  </a>
+			</Card.Content>
+		  </Card>
+		)
+	}
+}
+
+export default PlayCard;
